08a3d3140567300c3f28b60532916a529b3cb779
Sakib Ahmad Khan Kashi <Sakib.Kashi@Triconinfotech.com>
2023-06-23 02:16:21 +0530

    basic structoring

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,66 @@
+'use client';
 import Link from "next/link";
+import './page.css'
+import '../globals.css'
+import '../../../public/Illustration.svg'
+import { useState } from "react";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+library.add(faEye, faEyeSlash)
+export default function Login() {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const togglePasswordVisibility = (e: any) => {
+        e.preventDefault();
+        setPasswordVisible(!passwordVisible);
+    };
 
 
-export default function Login(){
     return (
-        <div>
-            <h1>Login page</h1>
-            <Link href={'/'}>Home</Link>
+        <div className="container">
+            <div className="left-panel">
+                <div className="logo-container">
+                    <img className="logo" src='/Logo.svg' />
+                    <h1 className="heading">Log In</h1>
+                </div>
+                <form className="main-form">
+                    <div>
+                        <label className="labels">Email Address</label>
+                        <input type="text"
+                            placeholder="Email Address"
+                        />
+                    </div>
+                    <div className="password-toggle">
+                        <label className="labels">Password</label>
+                        <input
+                            type={passwordVisible ? 'password' : 'text'}
+                            placeholder="password"
+                        />
+                        <span className="toggle-icon" onClick={togglePasswordVisibility}>
+                            {
+                                <FontAwesomeIcon
+                                    icon={passwordVisible ? "eye-slash" : "eye"}
+                                    onClick={() => setPasswordVisible(!passwordVisible)}
+                                />
+                            }
+                        </span>
+                    </div>
+                    <div className="rememberMe-checkbox">
+                        <input
+                            type="checkbox"
+                        // checked={checked}
+                        // onChange={handleChange}
+                        />
+                        <span className="message-tag">Remember Me</span>
+                    </div>
+                    <button className="page-btn">Login</button>
+                    <Link className="reset-btn link-btn" href={'/'}>Reset Password?</Link>
+                </form>
+                <span className="line-message">Don't have a account yet ?<Link className="link-btn" href={'/signup'}>New Account</Link></span>
+            </div>
+            <div className="right-panel">
+                <img className="illustrate" src='/illustration.svg' />
+            </div>
         </div>
     )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,11 +1,95 @@
+'use client';
 import Link from "next/link";
+import '../globals.css'
+import './signup.css';
+import { useState } from "react";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+library.add(faEye, faEyeSlash)
 
+export default function SignUp() {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [isEmailValid, setIsEmailValid] = useState(true);
+    const [email, setEmail] = useState('');
+    const togglePasswordVisibility = (e: any) => {
+        e.preventDefault();
+        setPasswordVisible(!passwordVisible);
+    };
 
-export default function SignUp(){
+    function handleEmailChange(e: any) {
+        e.preventDefault();
+        setEmail(e.target.value);
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        setIsEmailValid(emailRegex.test(email));
+    }
     return (
         <div>
-            <h1>SignUp page</h1>
-            <Link href={'/'}>Home</Link>
+            {/* <h1>SignUp page</h1>
+            <Link href={'/'}>Home</Link> */}
+            <div className="container">
+                <div className="left-panel">
+                    <div className="logo-container">
+                        <img className="logo" src='/Logo.svg' />
+                        <h1 className="heading">Sign Up</h1>
+                    </div>
+                    <form className="main-form">
+                        <div>
+                            <label className="labels">Full Name</label>
+                            <input type="text"
+                                placeholder="Full Name"
+                            />
+                        </div>
+                        <div>
+                            <label className="labels">Email Address</label>
+                            <input type="text"
+                                placeholder="Email Address"
+                                value={email}
+                                onChange={handleEmailChange}
+                            />
+                            {!isEmailValid && email !== '' && (
+                                <span className="error-message">Please enter a valid email address.</span>
+                            )}
+                        </div>
+                        <div>
+                            <label className="labels">Username</label>
+                            <input type="text"
+                                placeholder="Username"
+                            />
+                        </div>
+                        <div className="password-toggle">
+                            <label className="labels">Password</label>
+                            <input
+                                type={passwordVisible ? 'password' : 'text'}
+                                placeholder="password"
+                            />
+                            <span className="toggle-icon" onClick={togglePasswordVisibility}>
+                                {
+                                    <FontAwesomeIcon
+                                        icon={passwordVisible ? "eye-slash" : "eye"}
+                                        onClick={() => setPasswordVisible(!passwordVisible)}
+                                    />
+                                }
+                            </span>
+                        </div>
+                        <div className="inline-checkbox">
+                            <input
+                                type="checkbox"
+                            // checked={checked}
+                            // onChange={handleChange}
+                            />
+                            <span className="message-style">
+                                By creating an account you agree to the <Link className="link-btn" href={''}>terms of use</Link> and our <Link className="link-btn" href={''}>privacy policy.</Link>
+                            </span>
+                        </div>
+                        <button className="page-btn">Create Account</button>
+                    </form>
+                    <span className="line-message">Already have an account? <Link className="link-btn" href={'/login'}>Login</Link></span>
+                </div>
+                <div className="right-panel">
+                    <img className="signup-illustrate" src='/signUp.svg' />
+                </div>
+            </div>
         </div>
     )
 }
